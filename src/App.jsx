0ddf75c9da1170017578ee438fbc8917e846b647
masinde sylvester <masinde.sylvester@yahoo.com>
2025-01-29 22:02:12 +0300

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
       
 
       {/* Hero */}
-      <section className="bg-indigo-700 py-20 mb-4">
+      <section className="bg-sky-900 py-20 mb-4">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
           <div className="text-center">
             <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
-
+import logo from '../assets/images/logo.png';
 
 const Navbar = () => {
   return (
-    <nav className="bg-indigo-700 border-b border-indigo-500">
+    <nav className="bg-sky-700 border-b border-white-500">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="flex h-20 items-center justify-between">
             <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
               {/* Logo */}
               <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
-                <img className="h-10 w-auto" src="images/logo.png" alt="React Jobs" />
+                <img className="h-10 w-auto" src={logo} alt="React Jobs" />
                 <span className="hidden md:block text-white text-2xl font-bold ml-2">
                 Find Your Home
                 </span>
@@ -19,10 +19,10 @@ const Navbar = () => {
                     Home
                   </a>
                   <a href="/jobs.html" className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
-                    Jobs
+                    Property
                   </a>
                   <a href="/add-job.html" className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
-                    Add Job
+                    Add Property
                   </a>
                 </div>
               </div>
