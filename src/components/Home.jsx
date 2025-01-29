d713ec6ce4bd3bc6ed9d2659cc886079669d8fb2
masinde sylvester <masinde.sylvester@yahoo.com>
2025-01-30 02:31:39 +0300

    initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -66,7 +66,7 @@ const Home = () => {
               </p>
               <Link
                 to="/add-tenant"
-                className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
+                className="inline-block bg-sky-500 text-white rounded-lg px-4 py-2 hover:bg-black"
               >
                 Add Tenant
               </Link>
@@ -80,7 +80,7 @@ const Home = () => {
               </p>
               <Link
                 to="/add-payment"
-                className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
+                className="inline-block bg-sky-500 text-white rounded-lg px-4 py-2 hover:bg-black"
               >
                 Add Payment
               </Link>
@@ -92,7 +92,7 @@ const Home = () => {
       {/* Browse Properties Section */}
       <section className="bg-blue-50 px-4 py-10">
         <div className="container-xl lg:container m-auto">
-          <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          <h2 className="text-3xl font-bold text-sky-500 mb-6 text-center">
             Browse Properties
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
@@ -106,7 +106,7 @@ const Home = () => {
                   <div className="mb-5">
                     <p>{property.description}</p>
                   </div>
-                  <h3 className="text-indigo-500 mb-2">Rent: ${property.rent}</h3>
+                  <h3 className="text-red-500 mb-2">Rent: ${property.rent}</h3>
                 </div>
               </div>
             ))}
